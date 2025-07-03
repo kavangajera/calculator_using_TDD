cf331347fd2456078ebefa8f1f120231dbc057bd
kavangajera <145152936+kavangajera@users.noreply.github.com>
2025-07-04 00:17:39 +0530

    🔴 Test: add('5') should return 5

--- tests/test_for_string_calculator.py
+++ tests/test_for_string_calculator.py
@@ -4,14 +4,19 @@ from src.string_calculator import StringCalculator
 class TestStringCalculator(unittest.TestCase):
     """Unit tests for the StringCalculator class."""
 
-    def setUp(self):
+    def setUp(self)->None:
         """Set up the StringCalculator instance for testing."""
         self.calculator = StringCalculator()
     
-    def test_add_empty_string_returns_0(self):
+    def test_add_empty_string_returns_0(self)->None:
         """Test adding an empty string."""
         result = self.calculator.add("")
         self.assertEqual(result, 0)
+    
+    def test_add_single_number_returns_number(self)->None:
+        """Test adding a single number."""
+        result = self.calculator.add("5")
+        self.assertEqual(result, 5)
 
 if __name__ == '__main__':
     unittest.main()
